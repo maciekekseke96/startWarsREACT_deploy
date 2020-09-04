2301fdbfc3fc7ffdc4824588bdf21325a86d25dc
Maciej Mandzak <maciekekseke@gmail.com>
2020-09-04 10:34:35 +0200

    Updates

---
+++ precache-manifest.349e9c15fe37d3ab3455b5d3c46265e3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f36ed4f94d99c1ede1df29af1c8657d8",
+    "url": "/startWarsREACT_deploy/index.html"
+  },
+  {
+    "revision": "cbc308b6e1aa8e339ff2",
+    "url": "/startWarsREACT_deploy/static/css/main.f4b7585b.chunk.css"
+  },
+  {
+    "revision": "9452cf1c2c153cf8a5eb",
+    "url": "/startWarsREACT_deploy/static/js/2.603b2ce5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/startWarsREACT_deploy/static/js/2.603b2ce5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbc308b6e1aa8e339ff2",
+    "url": "/startWarsREACT_deploy/static/js/main.17d0caad.chunk.js"
+  },
+  {
+    "revision": "2597f382afc567e19691",
+    "url": "/startWarsREACT_deploy/static/js/runtime-main.aebde942.js"
+  },
+  {
+    "revision": "da45c4c0de0efd2c6e1fd463b04d3b3f",
+    "url": "/startWarsREACT_deploy/static/media/ARROW OPEN.da45c4c0.svg"
+  },
+  {
+    "revision": "79977f053d6f42e5e0575fa2c33b24cd",
+    "url": "/startWarsREACT_deploy/static/media/LOGO.79977f05.svg"
+  },
+  {
+    "revision": "3d287be580297c2255b636b22dd73376",
+    "url": "/startWarsREACT_deploy/static/media/loading.3d287be5.png"
+  }
+]);
